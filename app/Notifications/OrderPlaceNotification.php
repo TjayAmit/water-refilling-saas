592c6d925e477a15a0ad778db2664c3d75784fdb
= <=>
2026-01-10 15:37:08 +0800

    Add `OrderPlaceNotification` and `OutForDeliveryNotification` classes for handling order-related notifications

---
+++ app/Notifications/OrderPlaceNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OrderPlaceNotification extends Notification
+{
+    use Queueable;
+
+    protected Order $order;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line("Your order {$this->order->order_number} is placed")
+            ->action('Notification Action', url("/orders/{$this->order->id}"))
+            ->line('Thank you for trusting our service, we will contact you soon!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'order_id' => $this->order->id,
+            'order_number' => $this->order->order_number,
+            'total_amount' => $this->order->total_amount,
+            'message' => 'Order placed successfully'
+        ];
+    }
+}
